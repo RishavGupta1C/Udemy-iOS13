73bab04b370ec2e1153bc220a7115c9b84fbb68d
RishavGupta1C <91869768+RishavGupta1C@users.noreply.github.com>
2021-12-22 13:31:27 +0530

    Add files via upload

---
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/CalculateViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+    
+    var calculatorBrain = CalculatorBrain()
+
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weigthLabel: UILabel!
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heightLabel.text = String(format: "%.2f", sender.value) + "m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weigthLabel.text = String(Int(sender.value)) + "Kg"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        heightSlider.value = 1.5
+        weightSlider.value = 100
+    }
+    
+    @IBAction func calculatePressed(_ sender: Any) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        calculatorBrain.calculateBMI(h: height,w: weight)
+        
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultViewController  // Forced Downcasting done using as!
+            destinationVC.bmiValue = calculatorBrain.getBMIValue()
+            destinationVC.advice = calculatorBrain.getAdvice()
+            destinationVC.color = calculatorBrain.getColor()
+        }
+    }
+
+}
+
